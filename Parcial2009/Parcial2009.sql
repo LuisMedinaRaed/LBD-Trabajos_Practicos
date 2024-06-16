267bf8ab92831e1bc8eff5d3270c5042f9a586bd
Luis Medina Raed <luiseugeniomr@gmail.com>
2024-06-16 02:04:34 -0300

    Correccion Parcial 2009

--- Parcial2009/Parcial2009.sql
+++ Parcial2009/Parcial2009.sql
@@ -13,95 +13,82 @@
 -- ----------------------------------------------------------------------------------------------------------
 -- ----------------------------------------------------------------------------------------------------------
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Parcial2009.DM1
+--
+-- Date Created : Sunday, June 16, 2024 01:51:49
+-- Target DBMS : MySQL 8.x
+--
 
-CREATE SCHEMA IF NOT EXISTS Parcial2009 DEFAULT CHARACTER SET utf8 ;
+DROP DATABASE IF EXISTS Parcial2009;
+CREATE DATABASE Parcial2009;
+USE Parcial2009;
 
-CREATE TABLE IF NOT EXISTS Parcial2009.`Obras` (
-  `IdObra` INT(11) NOT NULL,
-  `NombreObra` VARCHAR(75) NOT NULL,
-  `DireccionObra` VARCHAR(75) NOT NULL,
-  `TotalHoras` INT(11) NOT NULL,
-  PRIMARY KEY (`IdObra`),
-  UNIQUE INDEX `NombreObra_UNIQUE` (`NombreObra` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE Cargos(
+    IdCargo    INT            NOT NULL,
+    Cargo      VARCHAR(50)    NOT NULL,
+    PRIMARY KEY (IdCargo),
+    UNIQUE INDEX UI_Cargo(Cargo)
+)ENGINE=INNODB
+;
 
-CREATE TABLE IF NOT EXISTS Parcial2009.`Empleados` (
-  `IdEmpleado` INT(11) NOT NULL,
-  `IdCargo` INT(11) NOT NULL,
-  `IdJefe` INT(11) NULL DEFAULT NULL,
-  `Apellidos` VARCHAR(50) NOT NULL,
-  `Nombres` VARCHAR(50) NULL DEFAULT NULL,
-  `Direccion` VARCHAR(75) NOT NULL,
-  `Telefono` CHAR(7) NOT NULL CHECK (Telefono REGEXP '^4[0-9]{6}$'),
-  PRIMARY KEY (`IdEmpleado`),
-  INDEX `fk_Empleados_Empleados1_idx` (`IdJefe` ASC) VISIBLE,
-  INDEX `fk_Empleados_Cargos1_idx` (`IdCargo` ASC) VISIBLE,
-  INDEX `fk_ApellidosNombres` (`Apellidos` ASC, `Nombres` ASC) INVISIBLE,
-  CONSTRAINT `fk_Empleados_Empleados1`
-    FOREIGN KEY (`IdJefe`)
-    REFERENCES Parcial2009.`Empleados` (`IdEmpleado`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Empleados_Cargos1`
-    FOREIGN KEY (`IdCargo`)
-    REFERENCES Parcial2009.`Cargos` (`IdCargo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE Empleados(
+    IdEmpleado    INT            NOT NULL,
+    IdCargo       INT            NOT NULL,
+    IdJefe        INT,
+    Apellidos     VARCHAR(50)    NOT NULL,
+    Nombres       VARCHAR(50),
+    Direccion     VARCHAR(75)    NOT NULL,
+    Telefono      CHAR(7)        NOT NULL CHECK (Telefono REGEXP '^4[0-9]{6}$'),
+    PRIMARY KEY (IdEmpleado),
+    INDEX IX_ApellidosNombres(Apellidos, Nombres),
+    INDEX IX_IdCargo(IdCargo),
+    INDEX IX_IdJefe(IdJefe),
+    FOREIGN KEY (IdCargo)
+    REFERENCES Cargos(IdCargo),
+    FOREIGN KEY (IdJefe)
+    REFERENCES Empleados(IdEmpleado)
+)ENGINE=INNODB
+;
 
-CREATE TABLE IF NOT EXISTS Parcial2009.`Cargos` (
-  `IdCargo` INT(11) NOT NULL,
-  `Cargo` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`IdCargo`),
-  UNIQUE INDEX `Cargo_UNIQUE` (`Cargo` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE Obras(
+    IdObra           INT            NOT NULL,
+    NombreObra       VARCHAR(75)    NOT NULL,
+    DireccionObra    VARCHAR(75)    NOT NULL,
+    TotalHoras       INT            NOT NULL,
+    PRIMARY KEY (IdObra),
+    UNIQUE INDEX UI_NombreObra(NombreObra)
+)ENGINE=INNODB
+;
 
-CREATE TABLE IF NOT EXISTS Parcial2009.`Trabajos` (
-  `IdObra` INT(11) NOT NULL,
-  `IdTrabajo` INT(11) NOT NULL,
-  `Descripcion` VARCHAR(100) NOT NULL,
-  `Horas` INT(11) NULL DEFAULT NULL,
-  `Estado` CHAR(1) NOT NULL CHECK (Estado IN ('A', 'B')),
-  PRIMARY KEY (`IdObra`, `IdTrabajo`),
-  INDEX `fk_Trabajos_Obras_idx` (`IdObra` ASC) VISIBLE,
-  CONSTRAINT `fk_Trabajos_Obras`
-    FOREIGN KEY (`IdObra`)
-    REFERENCES Parcial2009.`Obras` (`IdObra`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE Trabajos(
+    IdObra         INT             NOT NULL,
+    IdTrabajo      INT             NOT NULL,
+    Descripcion    VARCHAR(100)    NOT NULL,
+    Horas          INT,
+    Estado         CHAR(1)         NOT NULL CHECK (Estado IN ('A', 'B')),
+    PRIMARY KEY (IdObra, IdTrabajo),
+    INDEX IX_IdObra(IdObra),
+    FOREIGN KEY (IdObra)
+    REFERENCES Obras(IdObra)
+)ENGINE=INNODB
+;
 
-CREATE TABLE IF NOT EXISTS Parcial2009.`TrabajaEn` (
-  `IdObra` INT(11) NOT NULL,
-  `IdTrabajo` INT(11) NOT NULL,
-  `IdEmpleado` INT(11) NOT NULL,
-  `HorasTrabajadas` INT(11) NOT NULL,
-  PRIMARY KEY (`IdObra`, `IdTrabajo`, `IdEmpleado`),
-  INDEX `fk_TrabajaEn_Empleados1_idx` (`IdEmpleado` ASC) VISIBLE,
-  CONSTRAINT `fk_TrabajaEn_Trabajos1`
-    FOREIGN KEY (`IdObra` , `IdTrabajo`)
-    REFERENCES Parcial2009.`Trabajos` (`IdObra` , `IdTrabajo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_TrabajaEn_Empleados1`
-    FOREIGN KEY (`IdEmpleado`)
-    REFERENCES Parcial2009.`Empleados` (`IdEmpleado`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE TrabajaEn(
+    IdObra             INT    NOT NULL,
+    IdTrabajo          INT    NOT NULL,
+    IdEmpleado         INT    NOT NULL,
+    HorasTrabajadas    INT    NOT NULL,
+    PRIMARY KEY (IdObra, IdTrabajo, IdEmpleado),
+    INDEX IX_IdEmpleado(IdEmpleado),
+    INDEX IX_IdObraIdTrabajo(IdObra, IdTrabajo),
+    FOREIGN KEY (IdObra, IdTrabajo)
+    REFERENCES Trabajos(IdObra, IdTrabajo),
+    FOREIGN KEY (IdEmpleado)
+    REFERENCES Empleados(IdEmpleado)
+)ENGINE=INNODB
+;
 
 -- ----------------------------------------------------------------------------------------------------------
 -- ----------------------------------------------------------------------------------------------------------
@@ -114,7 +101,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 DELIMITER $$
 
-CREATE PROCEDURE Parcial2009.rsp_empleados_a_cargo(IN _IdEmpleado INT, OUT _Mensaje VARCHAR(255))
+CREATE PROCEDURE rsp_empleados_a_cargo(IN _IdEmpleado INT, OUT _Mensaje VARCHAR(255))
 BEGIN
     -- Comprobar si el empleado existe
     IF NOT EXISTS (SELECT 1 FROM Parcial2009.Empleados WHERE IdEmpleado = _IdEmpleado) THEN
@@ -153,15 +140,15 @@ END$$
 DELIMITER ;
 
 -- Llamada de prueba de éxito
-CALL Parcial2009.rsp_empleados_a_cargo(1, @Mensaje); -- El IdEmpleado 1 existe y tiene empleados a su cargo
+CALL rsp_empleados_a_cargo(1, @Mensaje); -- El IdEmpleado 1 existe y tiene empleados a su cargo
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: empleado no existe
-CALL Parcial2009.rsp_empleados_a_cargo(9999, @Mensaje); -- El IdEmpleado 9999 no existe
+CALL rsp_empleados_a_cargo(9999, @Mensaje); -- El IdEmpleado 9999 no existe
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: empleado no tiene empleados a su cargo
-CALL Parcial2009.rsp_empleados_a_cargo(4, @Mensaje); -- El IdEmpleado 4 existe pero no tiene empleados a su cargo
+CALL rsp_empleados_a_cargo(4, @Mensaje); -- El IdEmpleado 4 existe pero no tiene empleados a su cargo
 SELECT @Mensaje;
 
 
@@ -173,12 +160,14 @@ SELECT @Mensaje;
 -- ----------------------------------------------------------------------------------------------------------
 -- ----------------------------------------------------------------------------------------------------------
 
-CREATE VIEW Parcial2009.vista_trabajos AS
+CREATE VIEW vista_trabajos AS
 SELECT O.NombreObra, T.Descripcion, T.Horas, SUM(TE.HorasTrabajadas) as 'Total HorasTrabajadas'
 FROM Parcial2009.Obras O
 JOIN Parcial2009.Trabajos T ON O.IdObra = T.IdObra
 JOIN Parcial2009.TrabajaEn TE ON T.IdObra = TE.IdObra AND T.IdTrabajo = TE.IdTrabajo
 GROUP BY O.NombreObra, T.Descripcion, T.Horas;
+
+SELECT * FROM vista_trabajos;
 
 
 -- ----------------------------------------------------------------------------------------------------------
@@ -190,7 +179,7 @@ GROUP BY O.NombreObra, T.Descripcion, T.Horas;
 -- ----------------------------------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE PROCEDURE Parcial2009.rsp_alta_empleado(
+CREATE PROCEDURE rsp_alta_empleado(
     IN _IdEmpleado INT,
     IN _IdCargo INT,
     IN _IdJefe INT,
@@ -202,13 +191,13 @@ CREATE PROCEDURE Parcial2009.rsp_alta_empleado(
 )
 BEGIN
     -- Comprobar si el empleado ya existe
-    IF EXISTS (SELECT 1 FROM Parcial2009.Empleados WHERE IdEmpleado = _IdEmpleado) THEN
+    IF EXISTS (SELECT 1 FROM Empleados WHERE IdEmpleado = _IdEmpleado) THEN
         SET _Mensaje = 'Error: El empleado ya existe.';
     -- Comprobar si el cargo existe
-    ELSEIF NOT EXISTS (SELECT 1 FROM Parcial2009.Cargos WHERE IdCargo = _IdCargo) THEN
+    ELSEIF NOT EXISTS (SELECT 1 FROM Cargos WHERE IdCargo = _IdCargo) THEN
         SET _Mensaje = 'Error: El cargo no existe.';
     -- Comprobar si el jefe existe
-    ELSEIF NOT EXISTS (SELECT 1 FROM Parcial2009.Empleados WHERE IdEmpleado = _IdJefe) THEN
+    ELSEIF NOT EXISTS (SELECT 1 FROM Empleados WHERE IdEmpleado = _IdJefe) THEN
         SET _Mensaje = 'Error: El jefe no existe.';
     -- Comprobar si el número de teléfono es válido
     ELSEIF NOT _Telefono REGEXP '^4[0-9]{6}$' THEN
@@ -225,21 +214,21 @@ END$$
 DELIMITER ;
 
 -- Llamada de prueba de éxito
-CALL Parcial2009.rsp_alta_empleado(100, 1, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdEmpleado 100 no existe y el IdCargo 1 y IdJefe 1 existen
+CALL rsp_alta_empleado(100, 1, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdEmpleado 100 no existe y el IdCargo 1 y IdJefe 1 existen
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: empleado ya existe
-CALL Parcial2009.rsp_alta_empleado(1, 1, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdEmpleado 1 ya existe
+CALL rsp_alta_empleado(1, 1, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdEmpleado 1 ya existe
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: cargo no existe
-CALL Parcial2009.rsp_alta_empleado(101, 9999, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdCargo 9999 no existe
+CALL rsp_alta_empleado(101, 9999, 1, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdCargo 9999 no existe
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: jefe no existe
-CALL Parcial2009.rsp_alta_empleado(101, 1, 9999, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdJefe 9999 no existe
+CALL rsp_alta_empleado(101, 1, 9999, 'Apellido', 'Nombre', 'Direccion', '4000000', @Mensaje); -- El IdJefe 9999 no existe
 SELECT @Mensaje;
 
 -- Llamada de prueba de error: número de teléfono no válido
-CALL Parcial2009.rsp_alta_empleado(101, 1, 1, 'Apellido', 'Nombre', 'Direccion', '5000000', @Mensaje); -- El número de teléfono no comienza con 4
+CALL rsp_alta_empleado(101, 1, 1, 'Apellido', 'Nombre', 'Direccion', '5000000', @Mensaje); -- El número de teléfono no comienza con 4
 SELECT @Mensaje;
